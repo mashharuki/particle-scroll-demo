8f26e789b73e4b9a611a60db79feefd4a2e746d7
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-02-05 22:04:08 +0900

    若干修正

--- src/App.tsx
+++ src/App.tsx
@@ -15,18 +15,9 @@ const App = () => {
   const { userInfo } = useAuthCore();
 
   const [balance, setBalance] = useState<string>("");
+  const [smartAccount, setSmartAccount] = useState<any>("");
+  const [customProvider, setCustomProvider] = useState<any>();
   const [smartAccountAddress, setSmartAccountAddress] = useState<string>("");
-
-  const smartAccount = new SmartAccount(provider, {
-    projectId: import.meta.env.VITE_APP_PROJECT_ID!,
-    clientKey: import.meta.env.VITE_APP_CLIENT_KEY!,
-    appId: import.meta.env.VITE_APP_APP_ID!,
-    aaOptions: {
-      simple: [{ chainId: ScrollSepolia.id, version: '1.0.0' }]
-    }
-  });
-
-  const customProvider = new ethers.providers.Web3Provider(new AAWrapProvider(smartAccount, SendTransactionMode.Gasless), "any");
 
   useEffect(() => {
     if (userInfo) {
@@ -35,10 +26,22 @@ const App = () => {
   }, [userInfo]);
 
   const fetchBalance = async () => {
+    const smartAccount = new SmartAccount(provider, {
+      projectId: import.meta.env.VITE_APP_PROJECT_ID!,
+      clientKey: import.meta.env.VITE_APP_CLIENT_KEY!,
+      appId: import.meta.env.VITE_APP_APP_ID!,
+      aaOptions: {
+        simple: [{ chainId: ScrollSepolia.id, version: '1.0.0' }]
+      },
+    });
+  
+    const customProvider = new ethers.providers.Web3Provider(new AAWrapProvider(smartAccount , SendTransactionMode.Gasless), "any");
     const balanceResponse = await customProvider.getBalance(await smartAccount.getAddress());
 
     setBalance(ethers.utils.formatEther(balanceResponse));
-    setSmartAccountAddress(await smartAccount.getAddress())
+    setSmartAccountAddress(await smartAccount.getAddress());
+    setCustomProvider(customProvider);
+    setSmartAccount(smartAccount)
   }
 
   const handleLogin = async (authType: any) => {
@@ -51,19 +54,31 @@ const App = () => {
   };
 
   const executeUserOp = async () => {
-    const signer = customProvider.getSigner();
-
     const tx = {
       to: "0x000000000000000000000000000000000000dEaD",
       value: ethers.utils.parseEther("0.001"),
+      data: "0x"
     };
 
-    const txResponse = await signer.sendTransaction(tx);
-    const txReceipt = await txResponse.wait();
+    console.log("tx:", tx)
 
-    notification.success({
-      message: txReceipt.transactionHash
-    })
+    try {
+      const userOpBundle = await smartAccount.buildUserOperation({ tx })
+      const userOp = userOpBundle.userOp;
+      const userOpHash = userOpBundle.userOpHash;
+
+      // send UserOp
+      const txHash = await smartAccount.sendUserOperation({ userOp, userOpHash });
+
+      notification.success({
+        message: txHash
+      })
+    } catch(err: any) {
+      console.error("err:", err)
+      notification.error({
+        message: err
+      })
+    }
   };
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -18,19 +18,26 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         projectId: import.meta.env.VITE_APP_PROJECT_ID!,
         clientKey: import.meta.env.VITE_APP_CLIENT_KEY!,
         appId: import.meta.env.VITE_APP_APP_ID!,
-        authTypes: [AuthType.email, AuthType.google, AuthType.twitter],
+        authTypes: [
+          AuthType.email, 
+          AuthType.google, 
+          AuthType.twitter,
+          AuthType.discord,
+          AuthType.facebook,
+          AuthType.github
+        ],
         themeType: 'dark',
         fiatCoin: 'USD',
         language: 'en',
         erc4337: {
-          name: "SIMPLE",
-          version: "1.0.0",
+          name: "BICONOMY",
+          version: "2.0.0",
         },
         wallet: {
           visible: true,
           customStyle: {
             supportChains: [ScrollSepolia]
-          }
+          },
         },
       }}
     >
